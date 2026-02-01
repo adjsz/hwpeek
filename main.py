159f623c82dbaa452386466fc63f07b623fda692
adjsz <robotdinosaur194@outlook.com>
2026-02-01 20:26:15 +0000

    Inital commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import platform
+import socket
+import psutil
+import subprocess
+
+mem = psutil.virtual_memory()
+disk = psutil.disk_usage("/")
+os_info = platform.freedesktop_os_release()
+
+def show_os():
+    info = platform.freedesktop_os_release()
+    print("OS:", info["PRETTY_NAME"])
+
+def show_hostname():
+    print("Hostname:", socket.gethostname())
+
+def show_logical_cores():
+    print("Threads:", psutil.cpu_count(logical=True))
+
+def show_physical_cores():
+    print("Cores:", psutil.cpu_count(logical=False))
+
+def show_cpu_usage():
+    print("CPU usage:", psutil.cpu_percent(interval=1), "%")
+
+def show_total_ram():
+    print("Total RAM:", round(mem.total / 1024**3, 2), "GB") # converts bytes (B) to gigabytes (GB)
+
+def show_used_ram():
+    print("Used RAM:", round(mem.used / 1024**3, 2), "GB") # converts bytes (B) to gigabytes (GB)
+
+def show_disk_total():
+    print("Disk total:", round(disk.total / 1024**3, 2), "GB") # converts bytes (B) to gigabytes (GB)
+
+def show_disk_free():
+    print("Disk free:", round(disk.free / 1024**3, 2), "GB") # converts bytes (B) to gigabytes (GB)
+
+def get_cpu_name():
+    with open("/proc/cpuinfo") as f:
+        for line in f:
+            if "model name" in line:
+                return line.split(":")[1].strip()
+        return "CPU not found"
+
+def show_cpu():
+    print("CPU:", get_cpu_name())
+
+def get_gpu_name():
+    result = subprocess.check_output(["lspci"], text=True)
+    for line in result.splitlines():
+        if "VGA" in line or "3D controller" in line:
+            return line.split(":", 1)[1].strip()
+    return "GPU not found"
+        
+def show_gpu():
+    print("GPU:", get_gpu_name())
+
+
+def show_specs():
+    print('Specs:')
+    print('---------')
+    show_os()
+    show_hostname()
+    show_cpu()
+    show_physical_cores()
+    show_logical_cores()
+    show_cpu_usage()
+    show_gpu()
+    show_total_ram()
+    show_used_ram()
+    show_disk_total()
+    show_disk_free()
+
+def main():
+    show_specs()
+
+main()

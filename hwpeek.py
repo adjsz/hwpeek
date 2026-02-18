c3dd5109030215cee67389602f7c2d284047f135
adjsz <robotdinosaur@outlook.com>
2026-02-18 11:47:59 +0000

    Improved formatting in hwpeek.py

--- hwpeek.py
+++ hwpeek.py
@@ -9,31 +9,31 @@ GB = 1024**3 # converts bytes into gb
 
 def show_os():
     info = platform.freedesktop_os_release()
-    print("OS:", info["PRETTY_NAME"])
+    print(f"{'OS:':<12}{info['PRETTY_NAME']}")
 
 def show_hostname():
-    print("Hostname:", socket.gethostname())
+    print(f"{'Hostname:':<12}{socket.gethostname()}")
 
 def show_logical_cores():
-    print("Threads:", psutil.cpu_count(logical=True))
+    print(f"{'Threads:':<12}{psutil.cpu_count(logical=True)}")
 
 def show_physical_cores():
-    print("Cores:", psutil.cpu_count(logical=False))
+    print(f"{'Cores:':<12}{psutil.cpu_count(logical=False)}")
 
 def show_cpu_usage():
-    print("CPU usage:", psutil.cpu_percent(interval=1), "%")
+    print(f"{'CPU Usage:':<12}{psutil.cpu_percent(interval=1)}%")
 
 def show_total_ram():
-    print("Total RAM:", round(mem.total / GB, 2), "GB") # converts bytes (B) to gigabytes (GB)
+    print(f"{'Total RAM:':<12}{round(mem.total / GB, 2)} GB")
 
 def show_used_ram():
-    print("Used RAM:", round(mem.used / GB, 2), "GB") # converts bytes (B) to gigabytes (GB)
+    print(f"{'Used RAM:':<12}{round(mem.used / GB, 2)} GB")
 
 def show_disk_total():
-    print("Disk total:", round(disk.total / GB, 2), "GB") # converts bytes (B) to gigabytes (GB)
+    print(f"{'Disk Total:':<12}{round(disk.total / GB, 2)} GB")
 
 def show_disk_free():
-    print("Disk free:", round(disk.free / GB, 2), "GB") # converts bytes (B) to gigabytes (GB)
+    print(f"{'Disk Free:':<12}{round(disk.free / GB, 2)} GB")
 
 def get_cpu_name():
     with open("/proc/cpuinfo") as f:
@@ -43,18 +43,20 @@ def get_cpu_name():
         return "CPU not found"
 
 def show_cpu():
-    print("CPU:", get_cpu_name())
+    print(f"{'CPU:':<12}{get_cpu_name()}")
 
 def get_gpu_name():
-    result = subprocess.check_output(["lspci"], text=True)
-    for line in result.splitlines():
-        if "VGA" in line or "3D controller" in line:
-            return line.split(":", 1)[1].strip()
+    try:
+        result = subprocess.check_output(["lspci"], text=True)
+        for line in result.splitlines():
+            if "VGA" in line or "3D controller" in line:
+                return line.split(":", 1)[1].strip()
+    except FileNotFoundError:
+        return "lspci not found"
     return "GPU not found"
-        
-def show_gpu():
-    print("GPU:", get_gpu_name())
 
+def show_gpu():
+    print(f"{'GPU:':<12}{get_gpu_name()}")
 
 def show_specs():
     print('Specs:')

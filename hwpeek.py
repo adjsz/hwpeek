be6e2fe07af23557ae52e2f50aebb499cdc1178e
adjsz <robotdinosaur@outlook.com>
2026-03-04 22:59:04 +0000

    Added new features, got rid of formatting

--- hwpeek.py
+++ hwpeek.py
@@ -2,38 +2,54 @@ import platform
 import socket
 import psutil
 import subprocess
+import getpass
 
+username = getpass.getuser()
+hostname = socket.gethostname()
+
+info = platform.freedesktop_os_release()
+kernel_version = platform.release()
+architecture = platform.machine()
 mem = psutil.virtual_memory()
 disk = psutil.disk_usage("/")
+logical_cores = psutil.cpu_count(logical=True)
+physical_cores = psutil.cpu_count(logical=False)
+cpu_usage = psutil.cpu_percent(interval=1)
 GB = 1024**3 # converts bytes into gb
 
+
 def show_os():
-    info = platform.freedesktop_os_release()
-    print(f"{'OS:':<12}{info['PRETTY_NAME']}")
+    print(f"{'OS: '}{info['PRETTY_NAME']}", architecture)
+
+def show_kernel_version():
+    print(f"{'Kernel version: '}{kernel_version}")
 
 def show_hostname():
-    print(f"{'Hostname:':<12}{socket.gethostname()}")
+    print(f"{'Hostname: '}{hostname}")
+
+def show_username():
+    print(f"{'Username: '}{username}")
 
 def show_logical_cores():
-    print(f"{'Threads:':<12}{psutil.cpu_count(logical=True)}")
+    print(f"{'Threads: ':}{logical_cores}")
 
 def show_physical_cores():
-    print(f"{'Cores:':<12}{psutil.cpu_count(logical=False)}")
+    print(f"{'Cores: ':}{physical_cores}")
 
 def show_cpu_usage():
-    print(f"{'CPU Usage:':<12}{psutil.cpu_percent(interval=1)}%")
+    print(f"{'CPU Usage: '}{cpu_usage}%")
 
 def show_total_ram():
-    print(f"{'Total RAM:':<12}{round(mem.total / GB, 2)} GB")
+    print(f"{'Total RAM: '}{round(mem.total / GB, 2)} GB")
 
 def show_used_ram():
-    print(f"{'Used RAM:':<12}{round(mem.used / GB, 2)} GB")
+    print(f"{'Used RAM: '}{round(mem.used / GB, 2)} GB")
 
 def show_disk_total():
-    print(f"{'Disk Total:':<12}{round(disk.total / GB, 2)} GB")
+    print(f"{'Disk Total: '}{round(disk.total / GB, 2)} GB")
 
 def show_disk_free():
-    print(f"{'Disk Free:':<12}{round(disk.free / GB, 2)} GB")
+    print(f"{'Disk Free: '}{round(disk.free / GB, 2)} GB")
 
 def get_cpu_name():
     with open("/proc/cpuinfo") as f:
@@ -43,7 +59,7 @@ def get_cpu_name():
         return "CPU not found"
 
 def show_cpu():
-    print(f"{'CPU:':<12}{get_cpu_name()}")
+    print(f"{'CPU: '}{get_cpu_name()}")
 
 def get_gpu_name():
     try:
@@ -52,17 +68,19 @@ def get_gpu_name():
             if "VGA" in line or "3D controller" in line:
                 return line.split(":", 1)[1].strip()
     except FileNotFoundError:
-        return "lspci not found"
+        return "Error: lspci not found"
     return "GPU not found"
 
 def show_gpu():
-    print(f"{'GPU:':<12}{get_gpu_name()}")
+    print(f"{'GPU: '}{get_gpu_name()}")
 
 def show_specs():
     print('Specs:')
     print('---------')
     show_os()
+    show_kernel_version()
     show_hostname()
+    show_username()
     show_cpu()
     show_physical_cores()
     show_logical_cores()
